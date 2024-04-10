1d6524bfa73637dcad1cc336028735c6edc6d187
Yumi-Lab <nicolas@yumi-lab.com>
2024-04-10 02:05:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,34 @@
 # Mettre à jour le système
 sudo apt update && sudo apt upgrade -y
 
-# Installer les dépendances nécessaires
-sudo apt install -y python3 python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential
+# Installer les dépendances système nécessaires
+sudo apt install -y python3 python3-venv python3-pip libffi-dev libssl-dev autoconf build-essential cargo
+
+# Vérifier et créer le groupe gpio si nécessaire
+if [ ! $(getent group gpio) ]; then
+    sudo groupadd gpio
+    echo "Group gpio created."
+fi
 
 # Créer un utilisateur pour Home Assistant sans dossier home mais avec un shell
-sudo useradd -rm homeassistant -G dialout,gpio,i2c
+if [ ! $(getent passwd homeassistant) ]; then
+    sudo useradd -rm homeassistant -G dialout,i2c,gpio
+    echo "User homeassistant created successfully."
+else
+    echo "User homeassistant already exists."
+fi
 
 # Créer un dossier pour l'installation de Home Assistant et attribuer les permissions
 sudo mkdir -p /srv/homeassistant
 sudo chown homeassistant:homeassistant /srv/homeassistant
 
-# Passer à l'utilisateur homeassistant et créer un environnement virtuel
+# Installer Home Assistant dans un environnement virtuel
 sudo -u homeassistant -H -s <<EOF
 cd /srv/homeassistant
 python3 -m venv .
 source bin/activate
-python3 -m pip install wheel
-python3 -m pip install homeassistant
+pip install --upgrade pip setuptools wheel
+pip install homeassistant
 deactivate
 EOF
 
@@ -31,8 +42,8 @@ After=network-online.target
 
 [Service]
 Type=simple
-User=%i
-ExecStart=/srv/homeassistant/bin/hass -c "/home/%i/.homeassistant"
+User=homeassistant
+ExecStart=/srv/homeassistant/bin/hass -c "/home/homeassistant/.homeassistant"
 
 [Install]
 WantedBy=multi-user.target
@@ -41,4 +52,4 @@ EOF
 # Activer et démarrer le service Home Assistant
 sudo systemctl enable --now home-assistant@homeassistant
 
-echo "Home Assistant installation is complete. You can access it via your web browser at http://<your-pi-ip-address>:8123"
+echo "Installation de Home Assistant terminée. Vous pouvez y accéder via votre navigateur web à l'adresse http://<votre-adresse-ip>:8123"
